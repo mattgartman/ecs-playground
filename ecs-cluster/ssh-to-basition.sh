7aadefdaf7817a998dbd22b6ef6edf6b4f30b75a
Matt Gartman <mattgartman@gmail.com>
2016-11-27 13:22:27 -0500

    adding EFS into ECS

--- ecs-cluster/ssh-to-basition.sh
+++ ecs-cluster/ssh-to-basition.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+ssh-add ../../mrgEcsKeyPair.pem
 ssh -A -i ../../mrgEcsKeyPair.pem ec2-user@$(aws ec2 describe-instances --instance-id i-0f96ff6ccaf48888c --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
