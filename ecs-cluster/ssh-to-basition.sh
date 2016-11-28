5575f74305cec74e7af04d9d0418db26e400d922
Matt Gartman <mattgartman@gmail.com>
2016-11-28 13:42:04 -0500

    Updates to fix CF issues

--- ecs-cluster/ssh-to-basition.sh
+++ ecs-cluster/ssh-to-basition.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+eval $(ssh-agent -s)
 ssh-add ~/keys/mrgEcsKeyPair.pem
-ssh -A -i ~/keys/mrgEcsKeyPair.pem ec2-user@$(aws ec2 describe-instances --instance-id i-0f96ff6ccaf48888c --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+ssh -A -i ~/keys/mrgEcsKeyPair.pem ec2-user@$(aws ec2 describe-instances --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
