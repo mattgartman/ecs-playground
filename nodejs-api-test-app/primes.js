ee4b9c2342ffb42ce19443453ec36573774b27a2
Matt Gartman <mattgartman@gmail.com>
2016-12-03 20:09:52 -0500

    updated prime to fix 4; may break again to play around with jasmine

--- nodejs-api-test-app/primes.js
+++ nodejs-api-test-app/primes.js
@@ -1,5 +1,5 @@
 exports.isPrime = function(number){
-    for(var i = 2; i < number/2; i++) {
+    for(var i = 2; i <= number/2; i++) {
         if(number % i === 0) {
             return false;
         }
